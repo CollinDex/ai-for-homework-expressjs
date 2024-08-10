dd6978beed768b88d3bfbfa60d3f40e14b9f71da
CollinDex <obetscollins@gmail.com>
2024-08-10 21:26:47 +0100

    fix: conversation route

--- src/routes/conv.ts
+++ src/routes/conv.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import { textPromptSchema } from '../validationSchema/text-prompt';
 import { validateData } from '../middleware/validationMiddleware';
-import { getAllConversations } from '../controllers/conversation';
-import { getTxtSolution } from "../controllers";
+import { startConversation, continueConversation, getAllConversations } from '../controllers/conversation';
 
 const conversationRouter = Router();
 
-conversationRouter.post('/start-conversation', validateData(textPromptSchema), getTxtSolution);
-conversationRouter.post('/continue-conversation?:id', validateData(textPromptSchema), getTxtSolution);
+conversationRouter.post('/start-conversation', validateData(textPromptSchema), startConversation);
+conversationRouter.post('/continue-conversation?:id', validateData(textPromptSchema), continueConversation);
 conversationRouter.get('/all-conversations', getAllConversations); 
 export { conversationRouter };
